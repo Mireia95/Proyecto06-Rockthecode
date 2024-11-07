d3a510be5059d12ec4c5ae8c96a1ab02f4a894d1
Mireia <mireia95.granzotto@gmail.com>
2024-11-07 20:58:51 +0100

    controller updateAutor v2 - usado Set
    Usado Set para chequear duplicados de mi array al pasarle el campo "books"

--- src/api/controllers/autor.js
+++ src/api/controllers/autor.js
@@ -53,11 +53,34 @@ const updateAutor = async (req, res, next) => {
     const oldAutor = await Autor.findById(id);
     const newAutor = new Autor(req.body);
     newAutor._id = id;
-    //!no eliminar datos ya presentes
     newAutor.books = [...oldAutor.books, ...req.body.books];
-    //!elimino datos duplicados, si hay, en el array
+    //!elimino datos duplicados, si hay, en el array:
+    /*1.creo un nuevo Set para chequear books duplicados. Ya que Set no funciona usando objetos (los id son objetos), tengo que hacer:
+    2. convierto los id en string usando un ciclo "for of", para wue Set los pueda leer
+    3.con Set chequeo estos id convertidos a string, y me aseguro de que no estén duplicados
+    4.creo un nuevo array llamado arrayFinalBooks donde pasaré los id originales (de tipo objeto) que NO estan duplicados
+       */
+    const idBooksSet = new Set(); //1
+    const arrayFinalBooks = []; //4
 
-    for (let i = 0; i < newAutor.books.length; i++) {
+    for (let id of newAutor.books) {
+      const idString = id.toString(); //2
+      const checkDuplicados = idBooksSet.has(idString); //devuelve true si ya tiene el id, devuelve FALSE si no lo tiene
+      if (!checkDuplicados) {
+        idBooksSet.add(idString); //añado a Set el id String
+        arrayFinalBooks.push(id); //añado el id original en el array final
+      } else {
+        console.log('Estas añadiendo un duplicado');
+      }
+    }
+    newAutor.books = arrayFinalBooks;
+    const updateAutor = await Autor.findByIdAndUpdate(id, newAutor, {
+      new: true
+    });
+    return res.status(200).json(updateAutor);
+
+    //---- CODIGO VIEJO chequear duplicados con ciclo FOR (sin usar Set)
+    /* for (let i = 0; i < newAutor.books.length; i++) {
       const book1 = newAutor.books[i]._id.toString();
       for (let j = i + 1; j < newAutor.books.length; j++) {
         const book2 = newAutor.books[j]._id.toString();
@@ -67,12 +90,7 @@ const updateAutor = async (req, res, next) => {
           console.log('Estás añadiendo un libro que ya está añadido');
         }
       }
-    }
-
-    const updateAutor = await Autor.findByIdAndUpdate(id, newAutor, {
-      new: true
-    });
-    return res.status(200).json(updateAutor);
+    } */
   } catch (error) {
     return res.status(400).json('Error en la petición PUT');
   }
